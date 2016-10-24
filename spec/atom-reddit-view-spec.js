3cff0af00d6a2816748173692db2f81836085570
Michael Meli <mjmeli@ncsu.edu>
2016-10-24 10:01:32 -0400

    removed old spec file pointing to deleted view

--- spec/atom-reddit-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomRedditView from '../lib/atom-reddit-view';
-
-describe('AtomRedditView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
