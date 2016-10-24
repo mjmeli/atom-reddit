3c0b75bbdcd9e46c4c8a02c9200b5d8b31295af8
Michael Meli <mjmeli@ncsu.edu>
2016-10-24 09:49:42 -0400

    initial commit

---
+++ spec/atom-reddit-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomRedditView from '../lib/atom-reddit-view';
+
+describe('AtomRedditView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
